850a606d957148ecce6708743c29fdc1a101efce
Beupha <fabien.stodel@laposte.net>
2024-02-10 00:07:16 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Cookies from "js-cookie";
+import ReactPaginate from "react-paginate";
 
 import Header from "./components/Header";
 
--- src/pages/CharacterPage/CharacterPage.jsx
+++ src/pages/CharacterPage/CharacterPage.jsx
@@ -1,21 +1,24 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import ReactPaginate from "react-paginate";
 
 import "./CharacterPage.css";
 
 export default function CharacterPage() {
   const [characterList, setCharacterList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("http://127.0.0.1:3000/character");
-
+        const response = await axios.get("http://127.0.0.1:3000/character", {
+          page: page,
+        });
         setCharacterList(response.data.results);
       } catch (error) {
-        console.log("CharacterPage error -->", error.response);
+        console.log("CharacterPage error -->", error);
       }
 
       setIsLoading(false);
@@ -28,6 +31,10 @@ export default function CharacterPage() {
   ) : (
     <main className="characterPage">
       <div className="search">Search</div>
+      {/* <button onClick={setPage((prevState) => prevState + 1)}>
+        Résultats suivants
+      </button> */}
+
       <div className="characterCard">
         {characterList.map((character) => {
           return (
@@ -35,6 +42,7 @@ export default function CharacterPage() {
               to={`/comics/${character._id}`}
               key={character._id}
               className="characterCard"
+              // onClick={handleClick()}
             >
               <div className="avatarAndName">
                 <span className="characterName">{character.name}</span>
